0cc6efbd12e3a73531a2eacb5b2a142138e9c4d6
EOjin Choi <ojinga32@gmail.com>
2023-06-25 22:42:07 +0900

    home

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
-import { useState } from "react";
-import axios from "axios";
+import NewsList from './components/NewsList';
 
-
-// 연습
-
-// 두번째 연습
-// 하이하이
-function App() {
-  const [data, setData] = useState(null);
-  const onClick = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos/1").then(response => {
-      setData(response.data);
-    });
-  };
-
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불어오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
-    </div>
-  )
-}
+const App = () => {
+  return <NewsList />;
+};
 
 export default App;
